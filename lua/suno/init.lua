4ff50f42e12206436b82ae4f22d1b7b9edfeee4a
Prerit Dayal <prerit714@gmail.com>
2024-09-01 17:02:40 +0530

    A working solution for the time being

--- lua/suno/init.lua
+++ lua/suno/init.lua
@@ -26,9 +26,49 @@ end
 
 -- Perform operations if cpp filetype is identified
 M.__handle_cpp = function()
+	local debounce_time = 500 -- In milliseconds
+	local last_save_time = 0
+	local temp_dir = "suno_temp"
+	local temp_exec = temp_dir .. "/temp_exec"
+	local input_file = "input.txt"
+	local output_file = "output.txt"
+
 	local build_and_run_cpp = function()
+		-- Full path to the current file
+		local current_file = vim.fn.expand("%:p")
+
+		-- Debounce logic
 		local current_time = vim.fn.reltimefloat(vim.fn.reltime())
-		print("[Suno] I am running: " .. current_time)
+
+		if current_time - last_save_time < debounce_time / 1000 then
+			-- Do not do the save and run task
+			return
+		end
+
+		last_save_time = current_time -- update the last save time in memory
+		print("[Suno] Compiling and running your c++ file ... :)")
+
+		-- Check if the g++ executable exists
+		if vim.fn.executable("g++") == 0 then
+			print("[Suno] g++ executable was not found! ensure its installation :|")
+			return
+		end
+
+		-- if the temp directory does not exist, create one
+		if vim.fn.isdirectory(temp_dir) == 0 then
+			vim.fn.mkdir(temp_dir, "p")
+		end
+
+		-- Define the command to build and run the c++ file
+		local build_command = string.format("g++ -std=c++17 -g %s -o %s", current_file, temp_exec)
+		local run_command = string.format("./%s < %s > %s 2>&1", temp_exec, input_file, output_file)
+
+		-- Execute the build and run command
+		vim.fn.system(build_command)
+		vim.fn.system(run_command)
+
+		-- Print the final message
+		print("[Suno] Done. OK :)")
 	end
 
 	vim.api.nvim_create_autocmd("BufWritePost", {
